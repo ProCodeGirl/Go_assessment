7dc73c74349c62755a4eede39645c636d8aa9078
Saumya Pal <55447750+ProCodeGirl@users.noreply.github.com>
2021-01-17 10:10:33 +0530

    Add files via upload

---
+++ gowiki/web.go
@@ -0,0 +1,13 @@
+package main
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "<html><h3>Hi there , I love %s!</h3></html>", r.URL.Path[1:])
+}
+func main () {
+	http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServe(":8000", nil))
+}
